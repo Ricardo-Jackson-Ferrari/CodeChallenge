84dc5f76e867b75253e294d0d1d24a1597c43f08
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-03-11 15:31:28 -0300

    modelando banco

--- apps/account/models.py
+++ apps/account/models.py
@@ -26,5 +26,25 @@ class User(AbstractUser):
         return self.email
 
 
-class UserNotActivated(Exception):
-    pass
+
+
+class SimpleUser(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(SimpleUser, on_delete=models.CASCADE)
+    code = models.CharField(max_length=12, blank=True)
+    recommended_by = models.ForeignKey(SimpleUser, on_delete=models.CASCADE, blank=True, null=True, related_name='ref_by')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> None:
+        return self.user
+    
+
+
+
+
+
